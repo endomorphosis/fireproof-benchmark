d5472defc344e6b3d70a96b841b0ca3dfa8b420e
endomorphosis <starworks5@gmail.com>
2024-04-27 01:24:07 -0700

    Update config.json with new swarm settings

--- src/fireproof-master.js
+++ src/fireproof-master.js
@@ -1,16 +1,224 @@
 import { fireproof } from '@fireproof/core/node'
+import { WebSocketServer } from 'ws'
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+import fs from 'fs'
 
-const db = fireproof('music-app')
+async function run(options = {}) {
+	const argv = require('minimist')(process.argv.slice(2))
+	let ipAddress
+	let dbAddress
+	let index
+	let chunkSize
+	let swarmName
+	let port
+	if (!argv.ipAddress && !Object.keys(options).includes('ipAddress')) {
+			ipAddress = "127.0.0.1"
+	} else if (Object.keys(options).includes('ipAddress')){
+			ipAddress = options.ipAddress
+	}
+	else if (argv.ipAddress) {
+			ipAddress = argv.ipAddress
+	}
+	
+	if (!argv.swarmName && !options.swarmName) {
+			console.error('Please provide a swarm Name');
+			process.exit(1);
+	}
+	else if (Object.keys(options).includes('swarmName')) {
+			swarmName = options.swarmName
+	}
+	else if (argv.swarmName) {
+			swarmName = argv.swarmName
+	}
+	
+	if (!argv.port && !Object.keys(options).includes('port')) {
+			console.error('Please provide a port number');
+			process.exit(1);
+	}else if (Object.keys(options).includes('port')) {
+			port = options.port
+	}else if (argv.port) {
+			port = argv.port
+	}
 
-await db.put({ _id: 'beyonce', name: 'Beyoncé', hitSingles: 29 })
+	if (!argv.chunkSize && !Object.keys(options).includes('chunkSize')) {
+			console.error('Please provide a chunk size');
+			process.exit(1);
+	}else if (Object.keys(options).includes('chunkSize')) {
+			chunkSize = options.chunkSize
+	}else if (argv.chunkSize) {
+			chunkSize = argv.chunkSize
+	}
 
-db.subscribe(async () => {
-  const topArtists = await db.query("hitSingles", { range: [30, Infinity] })
-  // redraw the UI with the new topArtists
-})
+	if (!argv.index && !Object.keys(options).includes('index')) {
+			console.error('Please provide an index');
+			process.exit(1);
+	}
+	else if (Object.keys(options).includes('index')) {
+			index = options.index
+	}
+	else if (argv.index) {
+			index = argv.index
+	}
 
-const beyonceDoc = await db.get('beyonce')
-beyonceDoc.hitSingles += 1
-await db.put(beyonceDoc)
+	process.on('SIGTERM', handleTerminationSignal);
+	process.on('SIGINT', handleTerminationSignal);
+	const id = index
+	const db = fireproof(swarmName+"-"+index+"-of-"+chunkSize)
 
-console.log('Beyoncé now has', beyonceDoc.hitSingles, 'hit singles')
+	let config_json
+	if (fs.existsSync('config.json') === false) {
+			fs.writeFileSync('config.json', JSON.stringify([]), 'utf8');
+	}
+
+	if (fs.existsSync('config.json') === true) {
+		config_json = fs.readFileSync('config.json', 'utf8');
+		config_json = JSON.parse(config_json);
+		let config_json_length = Object.keys(config_json).length;
+		while((config_json_length - 1) < parseInt(index)) {
+				config_json.push({});
+				config_json_length = Object.keys(config_json).length;
+		}
+		if (Object.keys(config_json[index]).includes("orbitdbAddress") === true) {
+				config_json[index]["firebasedbAddress"] = db.address.toString();
+		}
+		else {
+				config_json[index] = {
+						index: index,
+						ipAddress: ipAddress,
+						port: port,
+						swarmName: swarmName,
+						orbitdbAddress: db.address,
+						chunkSize: chunkSize
+				};
+		}
+		fs.writeFileSync('config.json', JSON.stringify(config_json), 'utf8');
+	}
+	//console.info(`running with db address ${db.address}`)
+	const wss = new WebSocketServer({ port: port })
+	wss.on('connection', (ws) => {
+		console.log('New WebSocket connection');
+		ws.on('message', (message) => {
+			message = JSON.parse(message.toString());
+			console.log('Received message:', message);
+			let method = Object.keys(message)[0];
+			let data = message[method];
+			// Handle WebSocket messages here
+			switch (method) {
+				case 'insert':
+					// Handle insert logic
+					let insertKey = data._id;
+					let insertValue = data.content;
+					console.log('Inserting data: ', insertKey, insertValue);
+					validate(insertValue).then((result) => {
+						if (result) {
+							db.put(data).then(() => {
+								console.log('Data inserted:', data);
+								ws.send('Data inserted');
+							}).catch((error) => {
+								console.error('Error inserting data:', error);
+								ws.send('Error inserting data');
+							});
+						}
+						else{
+							console.error('Data validation failed:', insertValue);
+							ws.send('Data validation failed');
+						}
+					});
+					break;
+				case 'update':
+					// Handle update logic
+					let updateKey = data._id;
+					let updateValue = data.content;
+					let updatedDoc = {_id: updateKey, content: updateValue};
+					let docToUpdate2 = db.query("_id", selectID).then((doc) => {	
+					}).catch((error) => {
+						console.error('Error selecting document:', error);
+						ws.send('Error selecting document');
+					})
+					let docToUpdate = db.get(updateKey).then((doc) => {
+						validate(updatedDoc).then((result) => {
+							db.put(updatedDoc).then(() => {
+								console.log('Data updated:', data);
+								ws.send('Data updates');
+							}).catch((error) => {
+								console.error('Error updating data:', error);
+								ws.send('Error updating data');
+							});
+						}).catch((error) => {
+							console.error('Error updating data:', error);
+							ws.send('Error updating data');
+						})
+					}).catch((error) => {
+							console.error('Error upfating document:', error);
+							ws.send('Error updating document');
+					});
+					break;
+				case 'select':
+					// Handle select logic
+					let selectID = data._id;
+					let docToSelect = db.query("_id", selectID).then((doc) => {
+						console.log('Selected document:', doc);
+						ws.send(JSON.stringify(doc));
+					}).catch((error) => {
+						console.error('Error selecting document:', error);
+						ws.send('Error selecting document');
+					})
+					break;
+				case 'delete':
+					// Handle delete by ID logic
+					let deleteId = data._id;
+					let docToDelete = db.query("_id", selectID).then((doc) => {
+							db.del(deleteId).then((deletedDoc) => {
+							console.log('Document deleted:', deletedDoc);
+							ws.send('Document deleted');
+						}).catch((error) => {
+							console.error('Error deleting document:', error);
+							ws.send('Error deleting document');
+						});
+					}).catch((error) => {
+						console.error('Error deleting document:', error);
+						ws.send('Error deleting document');
+					});
+					break;
+					default:
+						console.log('Unknown message:', message);
+						break;
+			}
+		});
+	});
+}
+
+async function handleTerminationSignal() {
+	console.info('received termination signal, cleaning up and exiting...');
+	await db.close()
+	await orbitdb.stop()
+	await ipfs.stop()
+	process.exit();
+}
+
+async function validate() {
+	return true;
+}
+
+async function test() {
+	let ipAddress = "127.0.0.1"
+	let dbAddress = undefined
+	let index = 1
+	let chunkSize = 64  
+	let swarmName    = "caselaw"
+	let port = 50000
+
+	let test = {
+			ipAddress: ipAddress,
+			dbAddress: dbAddress,
+			index: index,
+			chunkSize: chunkSize,
+			swarmName: swarmName,
+			port: port
+	}
+	return await run(test)
+}
+
+await test()
+//await run()
